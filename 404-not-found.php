156253501f93cd488bb0f195359a952c11e2a331
Budi Setiawan <bud1.setia1@gmail.com>
2019-07-03 01:31:56 +0700

    add page not found (404)

---
+++ 404-not-found.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="assets/img/favicon.ico">
+	<title>AXA Be Surprise - Congratulation</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+	<main role="main" style="padding-bottom: 0;" class="bg-flower bg-blue">
+		<section style="padding-bottom: 0;">
+			<?php include('logo-head.php'); ?>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12">
+						<h2 class="text-center cl-blue">Notifikasi</h2>
+					</div>
+					<div class="col-12">
+							<div class="content-wrap text-center align-items-center d-flex align-items-center" style="height: 480px; max-height: 100vw;">
+								<div class="inner-wrap" style="width: 100%;">
+									<!-- <img src="assets/img/icon/checkmark.png" alt="" class="img-fluid mb-4"> -->
+									<p class="cl-black mb-0"><b>Error: 404</b></p>
+									<p class="cl-black">Oops! Page not found.</p>
+								</div>
+						</div>
+					</div>
+				</div>
+				<div class="row justify-content-center">
+					<a href="#" class="d-inline-block btn red mb-3">Lihat Voucher</a>
+				</div>
+			</div>
+		</section>
+	</main>
+	<?php include('footer.php'); ?>
+
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="  crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/main.js"></script>
+</body>
+</html>
